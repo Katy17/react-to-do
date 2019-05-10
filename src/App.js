f9c50222960e863b8600dc9a908a4e6f4c9bd188
Katy Mesgarian <k.mesgarian@gmail.com>
2019-05-10 13:03:10 +0100

    checkpoint To-Do State and Props: Completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import React, { Component } from 'react';
  import ToDo from './components/ToDo.js';
 
  class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       todos: [
+         { description: 'Complete Bloc Checkpoint1', isCompleted: true },
+         { description: 'Complete Bloc Checkpoint2', isCompleted: false },
+         { description: 'Complete Bloc Checkpoint3', isCompleted: false }
+       ]
+     };
+   }
+
    render() {
      return (
        <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
         </ul>
        </div>
      );
